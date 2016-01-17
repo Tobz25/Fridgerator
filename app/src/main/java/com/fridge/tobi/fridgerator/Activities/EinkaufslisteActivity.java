54588027c662a86b9025b7cc02bec4b0ad8cbed2
JanDobler <doja1011@hs-karlsruhe.de>
2016-01-17 11:09:58 +0100

    Added Einkaufsliste

---
+++ app/src/main/java/com/fridge/tobi/fridgerator/Activities/EinkaufslisteActivity.java
@@ -0,0 +1,15 @@
+package com.fridge.tobi.fridgerator.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fridge.tobi.fridgerator.R;
+
+public class EinkaufslisteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_einkaufsliste);
+    }
+}
